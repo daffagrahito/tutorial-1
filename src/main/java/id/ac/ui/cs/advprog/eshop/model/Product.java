fc2d99e15c1118fdcf8b359df5d410754ab30c93
daffagrahito <muhammad.daffa23@ui.ac.id>
2024-02-06 11:03:18 +0700

    Update input validation for edit and create

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -18,4 +18,25 @@ public class Product {
     private String generateUniqueProductId() {
         return UUID.randomUUID().toString();
     }
+
+    public void setProductId(String productId) {
+        if (productId == null || productId.trim().isEmpty()) {
+            throw new IllegalArgumentException("Product ID tidak boleh null atau empty");
+        }
+        this.productId = productId;
+    }
+
+    public void setProductName(String productName) {
+        if (productName == null || productName.trim().isEmpty()) {
+            throw new IllegalArgumentException("Nama Product tidak boleh null atau empty");
+        }
+        this.productName = productName;
+    }
+
+    public void setProductQuantity(int productQuantity) {
+        if (productQuantity < 0) {
+            throw new IllegalArgumentException("Quantity Product tidak boleh negatif");
+        }
+        this.productQuantity = productQuantity;
+    }
 }
