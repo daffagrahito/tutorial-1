c1dd341bf4a74d87d467026cbe39276d9622a9cf
daffagrahito <muhammad.daffa23@ui.ac.id>
2024-02-13 23:47:00 +0700

    build: add sonarcloud workflows

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	jacoco
 	id("org.springframework.boot") version "3.2.2"
 	id("io.spring.dependency-management") version "1.1.4"
+	id("org.sonarqube") version "4.4.1.3373"
 }
 
 group = "id.ac.ui.cs.advprog"
@@ -74,4 +75,16 @@ tasks.test{
 
 tasks.jacocoTestReport{
 	dependsOn(tasks.test)
+
+	reports {
+		xml.required = true
+	}
+}
+
+sonar {
+	properties {
+		property("sonar.projectKey", "daffagrahito_tutorial-1")
+		property("sonar.organization", "daffagrahito")
+		property("sonar.host.url", "https://sonarcloud.io")
+	}
 }
