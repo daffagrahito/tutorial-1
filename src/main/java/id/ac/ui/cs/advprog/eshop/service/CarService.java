69b027ca651cb196ea2c445af254bff73f13d153
daffagrahito <muhammad.daffa23@ui.ac.id>
2024-02-15 20:46:22 +0700

    feat: add new service files

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarService.java
@@ -0,0 +1,17 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Car;
+
+import java.util.List;
+
+public interface CarService {
+    Car create(Car car);
+
+    List<Car> findAll();
+
+    Car findById(String carId);
+
+    void update(String carId, Car car);
+
+    void deleteCarById(String carId);
+}
